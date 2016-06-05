abcb864f7177456b563db525159cd2666ff0ceef
Joshua Holmes <JoshuaDanielHolmes1991@gmail.com>
2016-06-05 00:58:23 -0500

    Small commit before I reformat my drive

--- src/display.rs
+++ src/display.rs
@@ -2,3 +2,9 @@
 // Author: Joshua Holmes
 //
 
+/// The width of the virtual display.
+pub const VIRTUAL_DISPLAY_WIDTH: usize = 160;
+/// The height of the virtual display.
+pub const VIRTUAL_DISPLAY_HEIGHT: usize = 144;
+/// The scale at which we enlarge the virtual display during rendering.
+pub const DISPLAY_SCALE: u32 = 10;
--- src/memory.rs
+++ src/memory.rs
@@ -2,3 +2,5 @@
 // Author: Joshua Holmes
 //
 
+/// Total size of memory address space.
+pub const MEMORY_ADDRESS_SPACE: usize = 0x10000;
